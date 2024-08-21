20d2c938a20580e82b0ab12708bde53ae7c44849
amm926616 <amm926616@gmail.com>
2024-08-21 16:50:49 +0630

    updated with mirror video idea

--- easy_json.py
+++ easy_json.py
@@ -1,4 +1,4 @@
-import json 
+import json, os
 
 # open file, read value, edit value and close the file. I can't write these steps again and again. So, I create easy_json for it
 
@@ -15,3 +15,19 @@ def get_value(key, file_path):
 	with open(file_path, 'r') as f:
 		data = json.load(f)
 	return data[key]
+
+def check_file_path(file_path):
+	if os.path.isfile(file_path):
+		print("The file exists.")
+	else:
+		print("The file does not exist.")
+		last_position = {
+		"position": 0,
+		"file": file_path,
+		"timer_active": True,  # Save the timer status
+		"timer_set": 300000,
+		"alwaysOnTopState": True}
+  
+		with open(file_path, 'w') as f:
+			json.dump(last_position, f)
+  
